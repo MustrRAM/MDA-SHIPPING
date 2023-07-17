8ba340a31a57e188fc2c5d19adf60f1f59c99666
Mustafa Ramazanov <mustafa.ramazanov.2002@gmail.com>
2023-07-17 19:30:36 +0300

    repair scroll

--- js/main.js
+++ js/main.js
@@ -22,3 +22,21 @@ window.onclick = function(event){
     }
 }
 
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('#animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
